b037886610c443b6265fe8fdc5289579a770eb82
Peter-Lueis <peterlueis07@gmail.com>
2022-11-27 17:56:42 +0700

    c

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,13 +9,16 @@
                 <div class="h2">Contact</div>
             </div>
             <div class="col-10">
-                <div class="h2"> Store Addres :</div>
-                <div class="h5">Jakarta</div>
+                <div class="h2"> Store Address :</div>
+                <div class="h5">Jalan Pembagunan Baru Raya,</div>
+                <div class="h5">Kompleks Pertokoan Emerald Blok III/12</div>
+                <div class="h5">Bintaro, Tangerang Selatan</div>
+                <div class="h5">Indonesia</div>
                 <div class="h2">Open Daily</div>
                 <div class="h5"> 08.00 - 20.00</div>
                 <div class="h2">Contact</div>
-                <div class="h5">Phone : </div>
-                <div class="h5">Email : </div>
+                <div class="h5">Phone : 021-08899776655</div>
+                <div class="h5">Email : happybookstore@happy.com</div>
             </div>
         </div>
     </div>
